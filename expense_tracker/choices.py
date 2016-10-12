d06fddfaceda685c118b0031a798b67c70a4e3c7
Farhat Jahan <farhatjahan42.com>
2016-10-13 02:16:31 +0530

    choices areadded to model fields

---
+++ expense_tracker/choices.py
@@ -0,0 +1,20 @@
+
+PAID_FOR_CHOICE = (
+    ('Trip and Travel', 'Trip and Travel'),
+    ('Grocery', 'Grocery'),
+    ('Parlour', 'Parlour'),
+    ('Cook', 'Cook'),
+    ('Maid', 'Maid'),
+    ('Food and Drinks', 'Food and Drinks'),
+    ('Gas', 'Gas')
+    ('Others', 'Others'),
+)
+
+PAYMENT_MODE_CHOICE = (
+    ('Cash', 'Cash'),
+    ('Wallet', 'Wallet'),
+    ('Card', 'Card'),
+    ('Check', 'Check'),
+    ('NetBanking', 'NetBanking'),
+    ('Others', 'Others'),
+)
--- expense_tracker/models.py
+++ expense_tracker/models.py
@@ -1,27 +1,33 @@
 from django.db import models
+import datetime
+from django.contrib.auth.models import User
+from expense_tracker.choices import PAID_FOR_CHOICE, PAYMENT_MODE_CHOICE
 
-# Create your models here.
-class Users(models.Model):
-	created_date = models.DateField(default=datetime.datetime.now())
-	user = models.ForeignKey(User)
+class BaseModel(models.Model):
+	created_date = models.DateTimeField(auto_now_add=True)
+	modified_date = models.DateTimeField(auto_now=True)
+	
+	class Meta:
+		abstract = True
+
+
+class UserDetail(BaseModel):
+	user = models.OneToOneField(User)
 	address = models.CharField(max_length=30, blank=True)
 
 	class Meta:
-		db_table = "users"
+		db_table = "userdetail"
 
 
-class ExpenseDetail(models.Model):
-	user = models.ForeignKey(Users)
-	created_date = models.DateField(default=datetime.datetime.now())
+class ExpenseDetail(BaseModel):
+	user_detail = models.OneToOneField(User)
 	amount_spend = models.DecimalField(decimal_places=2)
-	paid_to = models.CharField(max_length=40)
-	paid_date = models.DateField()
-	description = models.CharField(max_length=60)
-	payment_mode = models.CharField(max_length=20)
-	balance = modeels.DecimalField(decimal_places=2)
+	paid_for = models.CharField(max_length=15,choices=PAID_FOR_CHOICE,default='Grocery')
+	paid_date = models.DateTimeField(default=datetime.datetime.now())
+	description = models.TextField()
+	payment_mode = models.CharField(max_length=10, choices=PAYMENT_MODE_CHOICE,default='Cash')
+	balance = models.DecimalField(decimal_places=2)
 
 	class Meta:
-       		db_table = "expences"
-
-
+		db_table = "expencedetail"
 
